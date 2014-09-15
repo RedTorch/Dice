5ac1bda228a2b41eaffa5bb5ec471d4472bd5b58
RedTorch <kenta.medina@gmail.com>
2014-09-15 11:22:42 -0700

    Added first name to index.html.

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 int credits = 1000;
 int bet = 10;
 int prob = 4;
-void setup()
+public void setup()
 {
 	noLoop();
 	size(400,400);
 }
-void draw()
+public void draw()
 {
 	background(0);
 	Die randy = new Die(60,160);
@@ -38,7 +54,7 @@ void draw()
     text("DECREASE BET BY 10",201,356);
     text("BUY 1 CHANCE ENHANCER",201,381);
 }
-void mousePressed()
+public void mousePressed()
 {
 	if(200 < mouseX && mouseX < 365)
 	{
@@ -79,7 +95,7 @@ class Die //models one single dice cube
 		myX = x;
 		myY = y;
 	}
-	void show()
+	public void show()
 	{
 		int one = 40;
 		int two = 40;
@@ -146,4 +162,13 @@ class Die //models one single dice cube
 		fill(seven,0,0);
 		ellipse(60+myX,60+myY,5,5);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
